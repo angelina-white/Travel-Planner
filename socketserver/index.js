d90e304b6a53062fe1305cf1db8cc7bc0110d95a
Angelina <angelinarwhite@gmail.com>
2022-07-21 12:52:57 -0400

    messages to and from socket server from docs

--- socketserver/index.js
+++ socketserver/index.js
@@ -9,12 +9,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
-  });
+  }); 
 
 server.listen(3001, () => {
   console.log('listening on *:3001');
